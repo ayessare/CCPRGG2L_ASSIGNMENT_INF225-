ca56f406746fbbbc0659617ed83c8f32699a2bbe
Alfred Christian Ricafort <122066185+ayessare@users.noreply.github.com>
2023-02-10 10:08:41 +0800

    Add files via upload

---
+++ Exercise5/Person.java
@@ -0,0 +1,25 @@
+public class Person {
+
+    private String name;
+    private int CCnumber;
+ 
+    public String getName() {
+ 
+        return this.name;
+    }
+    public int getCCnumber() {
+ 
+        return this.CCnumber;
+    }
+ 
+    public void setName(String newName) {
+ 
+        this.name = newName;
+    }
+ 
+    public void setCCnumber(int newCCnumber) {
+ 
+       this.CCnumber = newCCnumber;
+   }
+ }
+ 
